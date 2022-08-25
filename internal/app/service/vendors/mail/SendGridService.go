4ce3717733acf2e8e0a23f7a5f62dff94aebee47
Takafumi Iju <ijufumi@gmail.com>
2022-08-26 07:24:53 +0900

    Add implementation of SendGrid.

--- internal/app/service/vendors/mail/SendGridService.go
+++ internal/app/service/vendors/mail/SendGridService.go
@@ -1,13 +1,26 @@
 package mail
 
-import "github.com/ijufumi/email-service/internal/pkg/config"
+import (
+	"github.com/ijufumi/email-service/internal/pkg/config"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
 
 type sendGridService struct {
 	config *config.Config
 }
 
 func (service *sendGridService) Send(contents MailContents) error {
-	return nil
+	toAddress := mail.NewEmail(contents.ToAddress, contents.ToAddress)
+	fromAddress := mail.NewEmail(contents.FromAddress, contents.FromAddress)
+	message := mail.NewSingleEmailPlainText(fromAddress, contents.Subject, toAddress, contents.Body)
+	client := service.createClient()
+	_, err := client.Send(message)
+	return err
+}
+
+func (service *sendGridService) createClient() *sendgrid.Client {
+	return sendgrid.NewSendClient(service.config.Mail.SendGrid.SendGridAPIKEY)
 }
 
 func NewSendGridService(config *config.Config) MailVendorInterface {
