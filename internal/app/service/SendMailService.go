d9da71aafd5ac02b335dc1b9c00e1144430c1396
Takafumi Iju <ijufumi@gmail.com>
2022-08-25 19:37:34 +0900

    Add factory method

--- internal/app/service/SendMailService.go
+++ internal/app/service/SendMailService.go
@@ -1,1 +1,18 @@
 package service
+
+import "github.com/ijufumi/email-service/internal/app/service/vendors/mail"
+
+type SendMailServiceInterface interface {
+	Send(contents mail.MailContents) error
+}
+
+type sendMailService struct {
+}
+
+func (service *sendMailService) Send(contents mail.MailContents) error {
+	return nil
+}
+
+func NewSendMailService() SendMailServiceInterface {
+	return &sendMailService{}
+}
