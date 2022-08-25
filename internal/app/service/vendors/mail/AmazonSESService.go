e67932dbd1cccd07606a5a8dcccc0e5162355d8c
Takafumi Iju <ijufumi@gmail.com>
2022-08-25 15:43:15 +0900

    Add configuration

--- internal/app/service/vendors/mail/AmazonSESService.go
+++ internal/app/service/vendors/mail/AmazonSESService.go
@@ -1,12 +1,31 @@
 package mail
 
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/ijufumi/email-service/internal/pkg/config"
+)
+
 type amazonSESService struct {
+	config *config.Config
 }
 
 func (service *amazonSESService) Send(contents MailContents) error {
 	return nil
 }
 
-func NewAmazonSESService() MailVendorInterface {
-	return &amazonSESService{}
+func (service *amazonSESService) createSesService() (*ses.SES, error) {
+	sesSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(service.config.Mail.SES.AwsRegion),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return ses.New(sesSession), nil
+}
+
+func NewAmazonSESService(config *config.Config) MailVendorInterface {
+	return &amazonSESService{config: config}
 }
