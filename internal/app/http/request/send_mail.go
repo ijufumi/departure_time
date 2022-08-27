9b8e668fd84f7b9d9c77a2c21d1d2147d3eed1e8
Takafumi Iju <ijufumi@gmail.com>
2022-08-27 14:33:22 +0900

    Add validation tag

--- internal/app/http/request/send_mail.go
+++ internal/app/http/request/send_mail.go
@@ -2,8 +2,8 @@ package request
 
 // SendMail is request parameter for send mail
 type SendMail struct {
-	ToAddress   string `json:"to_address"`
-	FromAddress string `json:"from_address"`
-	Subject     string `json:"subject"`
-	Body        string `json:"body"`
+	ToAddress   string `json:"to_address" binding:"required,email"`
+	FromAddress string `json:"from_address" binding:"required,email"`
+	Subject     string `json:"subject" binding:"required"`
+	Body        string `json:"body" binding:"required"`
 }
