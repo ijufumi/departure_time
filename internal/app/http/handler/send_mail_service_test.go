b9bcce642d23ce8cce06647b0a01809631a7a2c7
Takafumi Iju <ijufumi@gmail.com>
2022-08-27 14:33:42 +0900

    Add error test case

--- internal/app/http/handler/send_mail_service_test.go
+++ internal/app/http/handler/send_mail_service_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 )
 
+// TestSendMailHandler_Send is normal test case
 func TestSendMailHandler_Send(t *testing.T) {
 	w := httptest.NewRecorder()
 	ginContext, _ := gin.CreateTestContext(w)
@@ -24,4 +25,72 @@ func TestSendMailHandler_Send(t *testing.T) {
 
 	asserts := assert.New(t)
 	asserts.Equal(200, w.Result().StatusCode)
+}
+
+// TestSendMailHandler_Send_ValidationError is simple validation error
+func TestSendMailHandler_Send_ValidationError(t *testing.T) {
+	w := httptest.NewRecorder()
+	ginContext, _ := gin.CreateTestContext(w)
+
+	jsonBody := strings.NewReader(`{}`)
+	req, _ := http.NewRequest("POST", "/", jsonBody)
+	ginContext.Request = req
+
+	mockSendMailService := mock.NewMockSendMailService(false)
+	sendMailHandler := NewSendMailHandler(mockSendMailService)
+	sendMailHandler.Send(ginContext)
+
+	asserts := assert.New(t)
+	asserts.Equal(400, w.Result().StatusCode)
+}
+
+// TestSendMailHandler_Send_ValidationMailFormatError_To is email format error ( to address )
+func TestSendMailHandler_Send_ValidationMailFormatError_To(t *testing.T) {
+	w := httptest.NewRecorder()
+	ginContext, _ := gin.CreateTestContext(w)
+
+	jsonBody := strings.NewReader(`{"to_address":"to@test","from_address":"from@test.com","subject": "test subject", "body": "test body"}`)
+	req, _ := http.NewRequest("POST", "/", jsonBody)
+	ginContext.Request = req
+
+	mockSendMailService := mock.NewMockSendMailService(false)
+	sendMailHandler := NewSendMailHandler(mockSendMailService)
+	sendMailHandler.Send(ginContext)
+
+	asserts := assert.New(t)
+	asserts.Equal(400, w.Result().StatusCode)
+}
+
+// TestSendMailHandler_Send_ValidationMailFormatError_From is email format error ( from address )
+func TestSendMailHandler_Send_ValidationMailFormatError_From(t *testing.T) {
+	w := httptest.NewRecorder()
+	ginContext, _ := gin.CreateTestContext(w)
+
+	jsonBody := strings.NewReader(`{"to_address":"to@test.jp","from_address":"from@test","subject": "test subject", "body": "test body"}`)
+	req, _ := http.NewRequest("POST", "/", jsonBody)
+	ginContext.Request = req
+
+	mockSendMailService := mock.NewMockSendMailService(false)
+	sendMailHandler := NewSendMailHandler(mockSendMailService)
+	sendMailHandler.Send(ginContext)
+
+	asserts := assert.New(t)
+	asserts.Equal(400, w.Result().StatusCode)
+}
+
+// TestSendMailHandler_Send_Error is error caused by send mail service
+func TestSendMailHandler_Send_Error(t *testing.T) {
+	w := httptest.NewRecorder()
+	ginContext, _ := gin.CreateTestContext(w)
+
+	jsonBody := strings.NewReader(`{"to_address":"to@test.jp","from_address":"from@test.com","subject": "test subject", "body": "test body"}`)
+	req, _ := http.NewRequest("POST", "/", jsonBody)
+	ginContext.Request = req
+
+	mockSendMailService := mock.NewMockSendMailService(true)
+	sendMailHandler := NewSendMailHandler(mockSendMailService)
+	sendMailHandler.Send(ginContext)
+
+	asserts := assert.New(t)
+	asserts.Equal(500, w.Result().StatusCode)
 }
