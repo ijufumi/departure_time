643a66da1ea86414f8f12a4c494c4468ae15e5fc
Takafumi Iju <ijufumi@gmail.com>
2022-09-03 15:37:30 +0900

    Fixed bug

--- web/src/repositories/RepositoryFactory.ts
+++ web/src/repositories/RepositoryFactory.ts
@@ -2,7 +2,7 @@ import SendMailRepository from "./SendMailRepository";
 import SendMailRepositoryImpl from "./impl/SendMailRepositoryImpl";
 
 class RepositoryFactory {
-  private static API_ENDPOINT = "";
+  private static API_ENDPOINT = "/api/v1";
 
   static createSendMailRepository():SendMailRepository {
     return new SendMailRepositoryImpl(this.API_ENDPOINT);
