25d147020425ba893ca06fc2dcde9c938c57e64c
Takafumi Iju <ijufumi@gmail.com>
2022-09-06 08:34:55 +0900

    Fixed bug

--- web/src/repositories/impl/SendMailRepositoryImpl.ts
+++ web/src/repositories/impl/SendMailRepositoryImpl.ts
@@ -3,7 +3,8 @@ import BaseRepository from "../BaseRepository";
 
 class SendMailRepositoryImpl extends BaseRepository implements SendMailRepository {
     send = async (args: { toAddress: string, fromAddress: string, subject: string, body: string }) => {
-        return this.post({ path: "/mail/send", body: args });
+        const { toAddress, fromAddress, subject, body} = args;
+        return this.post({ path: "/mail/send", body: {"to_address": toAddress, "from_address": fromAddress, subject, body} });
     }
 }
 
